bcbdd4c63b50de1c34fbf715f88d65d0b24aef27
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-18 04:58:46 -0700

    `wp core language` is now `wp language core`

--- language-command.php
+++ language-command.php
@@ -9,7 +9,7 @@ if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
 
-WP_CLI::add_command( 'core language', 'Core_Language_Command', array(
+WP_CLI::add_command( 'language core', 'Core_Language_Command', array(
 	'before_invoke' => function() {
 		if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
 			WP_CLI::error( "Requires WordPress 4.0 or greater." );
