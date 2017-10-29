305367414019c43b166319dc4cbf1c9ab318fa11
Alain Schlesser <alain.schlesser@gmail.com>
2017-10-29 12:34:57 +0100

    Add a "language" command namespace.
    This allows the addition of a description to show up in the help screen.
    
    See wp-cli/wp-cli#4385

---
+++ src/Language_Namespace.php
@@ -0,0 +1,20 @@
+<?php
+
+use WP_CLI\Dispatcher\CommandNamespace;
+
+/**
+ * Manage language files.
+ *
+ * ## EXAMPLES
+ *
+ *     # Install language
+ *     $ wp language core install nl_NL
+ *     Success: Language installed.
+ *
+ *     # Activate language
+ *     $ wp language core activate nl_NL
+ *     Success: Language activated.
+ */
+class Language_Namespace extends CommandNamespace {
+
+}
