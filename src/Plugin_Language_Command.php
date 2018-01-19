850f0714ff4be582df4388230317ad669455a72c
Pascal Birchler <hello@pascalbirchler.ch>
2018-01-19 20:31:38 +0100

    Remove unneeded code

--- src/Plugin_Language_Command.php
+++ src/Plugin_Language_Command.php
@@ -117,13 +117,6 @@ class Plugin_Language_Command extends WP_CLI\CommandWithTranslation {
 
 				$translation['update'] = $update ? 'available' : 'none';
 
-				$fields = array_keys( $translation );
-				foreach ( $fields as $field ) {
-					if ( isset( $assoc_args[ $field ] ) && $assoc_args[ $field ] !== $translation[ $field ] ) {
-						unset( $translation[ $key ] );
-					}
-				}
-
 				$translations[] = $translation;
 			}
 		}
