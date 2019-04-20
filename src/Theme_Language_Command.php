26d09bc65bffb22efbdf19ce20947e3eedc2aaac
jrfnl <jrfnl@users.noreply.github.com>
2019-04-20 20:28:47 +0200

    PHPCS: fix up the code base [2] - strict comparisons
    Fixes relate to the following rules:
    * Use the `$strict` parameter when calling `in_array()`.
    
    Notes regarding the changes for the comparison in the `Core_Language_Command` file:
    * I have verified that the contents of `$available` is expected to always be strings, so a strict comparison should be good here.
    * As the line became even longer now, I have taken the liberty to rewrite it to something slightly more readable.

--- src/Theme_Language_Command.php
+++ src/Theme_Language_Command.php
@@ -296,7 +296,7 @@ class Theme_Language_Command extends WP_CLI\CommandWithTranslation {
 			$assoc_args['format'] = 'table';
 		}
 
-		if ( in_array( $assoc_args['format'], array( 'json', 'csv' ) ) ) {
+		if ( in_array( $assoc_args['format'], array( 'json', 'csv' ), true ) ) {
 			$logger = new \WP_CLI\Loggers\Quiet();
 			\WP_CLI::set_logger( $logger );
 		}
