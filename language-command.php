025efb47738376b8cbef9752b0f80874e014248e
Juliette <663378+jrfnl@users.noreply.github.com>
2019-04-21 00:44:17 +0200

    QA: abstract a 3x duplicate closure to a function (#76)
    * QA: abstract a 3x duplicate closure to a function
    
    By the time the same code is used three times, it really shouldn't be a closure anymore, but a proper function.
    
    * Turn stand-alone function into a closure

--- language-command.php
+++ language-command.php
@@ -10,28 +10,22 @@ if ( file_exists( $wpcli_language_autoloader ) ) {
 	require_once $wpcli_language_autoloader;
 }
 
+$wpcli_language_check_requirements = function() {
+	if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
+		WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
+	}
+};
+
 WP_CLI::add_command( 'language core', 'Core_Language_Command', array(
-	'before_invoke' => function() {
-		if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
-			WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
-		}
-	})
+	'before_invoke' => $wpcli_language_check_requirements )
 );
 
 WP_CLI::add_command( 'language plugin', 'Plugin_Language_Command', array(
-		'before_invoke' => function() {
-			if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
-				WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
-			}
-		})
+		'before_invoke' => $wpcli_language_check_requirements )
 );
 
 WP_CLI::add_command( 'language theme', 'Theme_Language_Command', array(
-		'before_invoke' => function() {
-			if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
-				WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
-			}
-		})
+		'before_invoke' => $wpcli_language_check_requirements )
 );
 
 WP_CLI::add_hook( 'after_add_command:site', function () {
