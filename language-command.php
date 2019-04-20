ffe704012e0cee9bbeca0f546c944d5277d64789
jrfnl <jrfnl@users.noreply.github.com>
2019-04-20 20:03:53 +0200

    QA: abstract a 3x duplicate closure to a function
    By the time the same code is used three times, it really shouldn't be a closure anymore, but a proper function.

--- language-command.php
+++ language-command.php
@@ -11,27 +11,15 @@ if ( file_exists( $autoload ) ) {
 }
 
 WP_CLI::add_command( 'language core', 'Core_Language_Command', array(
-	'before_invoke' => function() {
-		if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
-			WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
-		}
-	})
+	'before_invoke' => wpcli_language_check_requirements())
 );
 
 WP_CLI::add_command( 'language plugin', 'Plugin_Language_Command', array(
-		'before_invoke' => function() {
-			if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
-				WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
-			}
-		})
+		'before_invoke' => wpcli_language_check_requirements())
 );
 
 WP_CLI::add_command( 'language theme', 'Theme_Language_Command', array(
-		'before_invoke' => function() {
-			if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
-				WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
-			}
-		})
+		'before_invoke' => wpcli_language_check_requirements())
 );
 
 WP_CLI::add_hook( 'after_add_command:site', function () {
@@ -40,4 +28,10 @@ WP_CLI::add_hook( 'after_add_command:site', function () {
 
 if ( class_exists( 'WP_CLI\Dispatcher\CommandNamespace' ) ) {
 	WP_CLI::add_command( 'language', 'Language_Namespace' );
+}
+
+function wpcli_language_check_requirements() {
+	if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
+		WP_CLI::error( 'Requires WordPress 4.0 or greater.' );
+	}
 }
