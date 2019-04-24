2a515db472480829dd2e598917f227af7670d301
Wojciech Smoliński <wojsmol@wp.pl>
2019-04-24 17:41:39 +0200

    update dockblock

--- src/WP_CLI/LanguagePackUpgrader.php
+++ src/WP_CLI/LanguagePackUpgrader.php
@@ -45,6 +45,7 @@ class LanguagePackUpgrader extends \Language_Pack_Upgrader {
 	 *
 	 * @param string $package The URI of the package. If this is the full path to an
 	 *                        existing local file, it will be returned untouched.
+	 * @param bool   $check_signatures Whether to validate file signatures. Default false.
 	 * @return string|\WP_Error The full path to the downloaded package file, or a WP_Error object.
 	 */
 	public function download_package( $package, $check_signatures = false ) {
