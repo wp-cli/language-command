deaeeafca28d141b086a777e647de4c8db50457e
Hiroshi Urabe <mail@torounit.com>
2020-08-24 14:07:46 +0900

    Update src/WP_CLI/LanguagePackUpgrader.php
    use short array syntax .
    
    Co-authored-by: Wojciech Smoliński <wojsmol@wp.pl>

--- src/WP_CLI/LanguagePackUpgrader.php
+++ src/WP_CLI/LanguagePackUpgrader.php
@@ -49,7 +49,7 @@ class LanguagePackUpgrader extends \Language_Pack_Upgrader {
 	 * @param array  $hook_extra       Extra arguments to pass to the filter hooks. Default empty array.
 	 * @return string|\WP_Error The full path to the downloaded package file, or a WP_Error object.
 	 */
-	public function download_package( $package, $check_signatures = false, $hook_extra = array() ) {
+	public function download_package( $package, $check_signatures = false, $hook_extra = [] ) {
 
 		/**
 		 * Filter whether to return the package.
