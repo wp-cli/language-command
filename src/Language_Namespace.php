d2a9453d40263ddd0fd95c523a94bf8fbc743ae5
Nilambar Sharma <nilambar@outlook.com>
2024-02-20 11:16:34 +0545

    Update examples for language command

--- src/Language_Namespace.php
+++ src/Language_Namespace.php
@@ -9,19 +9,39 @@ use WP_CLI\Dispatcher\CommandNamespace;
  *
  *     # Install the Dutch core language pack.
  *     $ wp language core install nl_NL
- *     Success: Language installed.
+ *     Downloading translation from https://downloads.wordpress.org/translation/core/6.4.3/nl_NL.zip...
+ *     Unpacking the update...
+ *     Installing the latest version...
+ *     Removing the old version of the translation...
+ *     Translation updated successfully.
+ *     Language 'nl_NL' installed.
+ *     Success: Installed 1 of 1 languages.
  *
  *     # Activate the Dutch core language pack.
- *     $ wp language core activate nl_NL
+ *     $ wp site switch-language nl_NL
  *     Success: Language activated.
  *
- *     # Install the Dutch language pack for Twenty Seventeen.
- *     $ wp language theme install twentyseventeen nl_NL
- *     Success: Language installed.
+ *     # Install the Dutch theme language pack for Twenty Ten.
+ *     $ wp language theme install twentyten nl_NL
+ *     Downloading translation from https://downloads.wordpress.org/translation/theme/twentyten/4.0/nl_NL.zip...
+ *     Unpacking the update...
+ *     Installing the latest version...
+ *     Removing the old version of the translation...
+ *     Translation updated successfully.
+ *     Language 'nl_NL' installed.
+ *     Success: Installed 1 of 1 languages.
  *
- *     # Install the Dutch language pack for Akismet.
- *     $ wp language plugin install akismet nl_NL
- *     Success: Language installed.
+ *     # Install the Dutch plugin language pack for Hello Dolly.
+ *     $ wp language plugin install hello-dolly nl_NL
+ *     Downloading translation from https://downloads.wordpress.org/translation/plugin/hello-dolly/1.7.2/nl_NL.zip...
+ *     Unpacking the update...
+ *     Installing the latest version...
+ *     Removing the old version of the translation...
+ *     Translation updated successfully.
+ *     Language 'nl_NL' installed.
+ *     Success: Installed 1 of 1 languages.
+ *
+ * @package wp-cli
  */
 class Language_Namespace extends CommandNamespace {
 
